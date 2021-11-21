6909b835de625b974251c7e1f410347f899bd6bf
Cerino Ligutom III <zeferinix@gmail.com>
2021-11-22 01:05:38 +0800

    feat: setup apollo for vue3

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,14 @@
-import { createApp } from 'vue';
-import App from './app.vue';
+import { createApp, provide, h } from "vue";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import App from "./app.vue";
+import { apolloClient } from "./plugins/apollo";
 
-createApp(App).mount('#app');
+const app = createApp({
+  setup() {
+    // https://v4.apollo.vuejs.org/guide-composable/setup.html#vue-3
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+});
+
+app.mount("#app");
